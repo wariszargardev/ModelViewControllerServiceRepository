2df2d74f2de6c2ec1d7a2d45f3d1a56eba8e026f
wariszargardev <wariszargardev@gmail.com>
2022-09-18 21:36:35 +0500

    initial setup for broadcasting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fired', function () {
+    event(new \App\Events\ChatMessage());
+    dd('Event fired.');
+});
